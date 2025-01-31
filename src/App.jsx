2c9aebe025b25e3017ea8ef79ad0dca841cb1f03
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-31 19:46:37 +0530

    set up weather data structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return ()
+	return (
+		<div>
+			<h1>My Weather Forecast</h1>
+			<p>Temperature: deg</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
