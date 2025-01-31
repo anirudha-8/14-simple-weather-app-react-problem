86e978c1b409b60111a8652bb14d0328e2daad2d
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-31 20:51:04 +0530

    improve error messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,45 @@ import { useState } from "react";
 
 function App() {
 	const [weatherData, setWeatherData] = useState(null);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		if (!navigator.geolocation) {
-			alert("Geolocation is not supported by your browser");
+			setError("Geolocation is not supported by your browser");
 			return;
 		}
-		navigator.geolocation.getCurrentPosition((position) => {
-			const latitude = position.coords.latitude;
-			const longitude = position.coords.longitude;
-			const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				const latitude = position.coords.latitude;
+				const longitude = position.coords.longitude;
+				const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
 
-			fetch(
-				`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
-			)
-				.then((res) => {
-					if (!res.ok) throw new Error("Failed to fetch the data!");
-					return res.json();
-				})
-				.then((data) => {
-					setWeatherData(data);
-					console.log(data);
-				})
-				.catch((error) => {
-					alert(error.message);
-				});
-		});
+				fetch(
+					`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+				)
+					.then((res) => {
+						if (!res.ok) throw new Error("Failed to fetch the data!");
+						return res.json();
+					})
+					.then((data) => {
+						setWeatherData(data);
+						console.log(data);
+					})
+					.catch((error) => {
+						setError(error.message);
+					});
+			},
+			(geoError) => setError(geoError.message)
+		);
 	}, []);
 
 	return (
 		<div>
 			<h1>My Weather Forecast</h1>
 			<div>
-				{weatherData ? (
+				{error ? (
+					<h1 style={{ color: "red" }}>Error: {error}</h1>
+				) : weatherData ? (
 					<div>
 						<h2>Your Location: {weatherData.name}</h2>
 						<p>Temperature: {weatherData.main.temp} °C</p>
